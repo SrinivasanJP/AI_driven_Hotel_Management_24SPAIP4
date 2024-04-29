9a015e1e899091b1e9ed0513491340d2b456be01
Srinivasan <jpsrinivasan38@gmail.com>
2024-04-29 10:59:46 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+    <h1 className=''>app</h1>
+   </div>
+  )
+}
+
+export default App
